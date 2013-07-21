7262e4892eba28a263df112fe3d989ccfee635d9
Laurion Burchall <admin@bernews.com>
2013-07-21 19:47:48 +0000

    openx-hack-check: look for new users

--- openx-hack-check.sql
+++ openx-hack-check.sql
@@ -1,3 +1,3 @@
 SELECT * FROM ox_01f6f2d3_banners WHERE append <> '' OR prepend <> '';
 SELECT * FROM ox_01f6f2d3_zones WHERE append <> '' OR prepend <> '';
-
+SELECT * FROM ox_01f6f2d3_users WHERE user_id > 3;
